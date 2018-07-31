0d2ab1c05ccfd62c46bb34357acb65228c0c6b81
wolfdigit <wolf_digit@hotmail.com>
2018-07-31 11:25:12 +0800

    Update userLogic.py

--- server-PC/userLogic.py
+++ server-PC/userLogic.py
@@ -5,5 +5,4 @@ value = None
 
 def onChange(nodeId, sensorId, value, db):
     ans = ""
-    ans += "{}\t{}\t{}\n".format(nodeId, (sensorId + 1), ((db[(nodeId, sensorId)].valueInt / 1000) % 2))
     return ans
